f61d542c4865a498b3a16f344fbf0b938aeb4021
Anthony Arias <aarias011@west-mec.org>
2019-08-16 17:13:42 -0700

    Finishes the second excercise:
    Able to open the photo in a second window.

--- sub-unit-01-DocumentObjectModel/zoom.js
+++ sub-unit-01-DocumentObjectModel/zoom.js
@@ -14,6 +14,7 @@
 var photoOrderArray = window.opener.photoOrder;
 var figFilename = "images/IMG_0" + photoOrderArray[2] + ".jpg";
 
+// var autoAdvance = setInterval(rightAdvance, 5000);
 /* populate img element and create event listener */
 function pageSetup() {
    document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
