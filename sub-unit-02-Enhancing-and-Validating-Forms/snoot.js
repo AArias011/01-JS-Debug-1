af19944ee9e7ca7d584ffc24706aae57c3b7ff2f
Anthony Arias <aarias011@west-mec.org>
2019-08-20 17:35:27 -0700

    Added many functions:
    Searched for syntax errors

--- sub-unit-02-Enhancing-and-Validating-Forms/snoot.js
+++ sub-unit-02-Enhancing-and-Validating-Forms/snoot.js
@@ -21,6 +21,53 @@ function setUpDays() {
     thirtyOne.appendChild(dates[30].cloneNode(true));
 }
 
+function updateDays() {
+    var deliveryDay = document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+
+    while(dates[28]){
+        deliveryDay.removeChild(dates[28]);
+    }
+    if (deliveryYear.selectedIndex === -1) {
+        deliveryYear.selectedIndex = 0;
+    }
+
+    if (selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex].value === "2020") {
+        deliveryDay.appendChild(twentyNine.cloneNode(true));
+    } else if(selectedMonth === "4" || selectedMonth === "6" || selectedMonth === "9" || selectedMonth === "11"){
+        deliveryDay.appendChild(thirty.cloneNode(true));
+    }else if (selectedMonth === "4" || selectedMonth === "6" || selectedMonth === "9" || selectedMonth === "11"){
+        deliveryDay.appendChild(thirty.cloneNode(true));
+    } else if (selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth === "7" || selectedMonth === "8" || selectedMonth === "10" || selectedMonth === "12") {
+        deliveryDay.appendChild(thirtyOne.cloneNode(true));
+    }
+}
+
+
+// run setUpPage() function when page finishes loading
+if (window.addEventListener) {
+    window.addEventListener("load", setUpPage,false);
+} else if (window.attachEvent){
+    window.attachEvent("onload", setUpPage);
+}
+
+var twentyNine = document.createDocumentFragment();
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
+
+function setUpDays() {
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+}
+
 
 if (window.addEventListener) {
     window.addEventListener("load", setUpPage, false);
@@ -36,7 +83,10 @@ function removeSelectDefaults() {
 
 function setUpPage() {
     removeSelectDefaults();
+    setUpDays();
+    createEventListener();
 }
+
 
 
 function createEventListener() {
@@ -48,6 +98,38 @@ function createEventListener() {
     }
 
     for (var i = 0; i < emptyBoxes.length; i++) {
+        emptyBoxes[i].selectedIndex = -1;
+    }
+    var deliveryMonth = document.getElementById("delivMo");
+    if (deliveryMonth.addEventListener) {
+        deliveryMonth.addEventListener("change", updateDays, false);
+    }else if (deliveryMonth.attachEvent){
+        deliveryMonth.attachEvent("onchange", updateDays);
+    }
+    
+    var deliveryYear = document.getElementById("delivYr");
+    if (deliveryYear.addEventListener) {
+        deliveryYear.addEventListener("change", updateDays, false);
+    } else if (deliveryYear.attachEvent) {
+        deliveryYear.attachEvent("onchange", updateDays);
+    }
+
+    var messageBox = document.getElementById("customText");
+    if (messageBox.addEventListener) {
+        messageBox.addEventListener("change", autoCheckCustom, false);
+    } else if (messageBox.attachEvent) {
+        messageBox.addEventListener("change", autoCheckCustom)
+    }
+}
+
+function createEventListener() {
+    var submitButton = document.getElementById("submit");
+    if (submitButton.addEventListener) {
+        submitButton.addEventListener("click", removeSelectDefaults, false);
+    } else if (submitButton.attachEvent) {
+        submitButton.attachEvent("onclick", removeSelectDefaults);
+    }
+    for(var i = 0; i < emptyBoxes.length; i++) {
         emptyBoxes[i].selectedIndex = -1;
     }
 }
